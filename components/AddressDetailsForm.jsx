5d4cde546caa05ad41b661676ac378e4945d71c9
Karishma <karishmaraina1664@gmail.com>
2025-04-23 15:26:36 +0530

    commit

--- components/AddressDetailsForm.jsx
+++ components/AddressDetailsForm.jsx
@@ -11,7 +11,7 @@ import {
   Dimensions,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import DateTimePicker from "@react-native-community/datetimepicker";
+// import DateTimePicker from "@react-native-community/datetimepicker";
 import { Dropdown } from "react-native-element-dropdown";
 
 
