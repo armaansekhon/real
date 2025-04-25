4ed706b080263275ac178e64d8b053175babb7a8
amt-12 <2020010004188@gndu.ac.in>
2025-04-25 15:38:41 +0530

    Update welcome message in AdminDashboard component

--- components/Adminhome.jsx
+++ components/Adminhome.jsx
@@ -111,7 +111,7 @@ const AdminDashboard = () => {
      style={styles.ani2}
    />
   </Animated.View>
-    Welcome <Text style={{ color:  '#5aaf57', fontFamily: 'PlusR', }}>Good Amrit</Text>
+    Welcome <Text style={{ color:  '#5aaf57', fontFamily: 'PlusR', }}>Admin</Text>
   </Animated.Text>
 </Animated.View>
 
